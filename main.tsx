78b91bd38a0a82642c71b683d54c9e370890379a
divya20-codes <divyadharshini20magesh@gmail.com>
2026-07-12 13:58:30 +0530

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,12 @@
+"use strict";
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=4881c661"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=4881c661"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.tsx?t=1783835951926";
+import "/src/index.css?t=1783835951926";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(App, { "data-replit-metadata": "artifacts/ecosphere/src/main.tsx:7:52", "data-component-name": "App" }, void 0, false, {
+  fileName: "/home/runner/workspace/artifacts/ecosphere/src/main.tsx",
+  lineNumber: 7,
+  columnNumber: 53
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQU1vRDtBQU5wRCxTQUFTQSxrQkFBa0I7QUFFM0IsT0FBT0MsU0FBUztBQUVoQixPQUFPO0FBRVBELFdBQVdFLFNBQVNDLGVBQWUsTUFBTSxDQUFFLEVBQUVDLE9BQU8sdUJBQUMsT0FBRSxpR0FBSDtBQUFBO0FBQUE7QUFBQTtBQUFBLE9BQUksQ0FBRyIsIm5hbWVzIjpbImNyZWF0ZVJvb3QiLCJBcHAiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VzIjpbIm1haW4udHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tICdyZWFjdC1kb20vY2xpZW50JztcblxuaW1wb3J0IEFwcCBmcm9tICcuL0FwcCc7XG5cbmltcG9ydCAnLi9pbmRleC5jc3MnO1xuXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykhKS5yZW5kZXIoPEFwcCAvPik7XG4iXSwiZmlsZSI6Ii9ob21lL3J1bm5lci93b3Jrc3BhY2UvYXJ0aWZhY3RzL2Vjb3NwaGVyZS9zcmMvbWFpbi50c3gifQ==
